81612609e5725470b64633d6c5fe584d7fbedd30
masyda.arrizaqu <arrizaqu@yahoo.com>
2016-03-02 22:13:41 +0700

    delete files

--- postgress_driver.java
+++
@@ -1,7 +0,0 @@
-# Example Postgress Driver
-
-Class.forName("org.postgresql.Driver");
-Connection connection = null;
-connection = DriverManager.getConnection(
-   "jdbc:postgresql://hostname:port/dbname","username", "password");
-connection.close();
