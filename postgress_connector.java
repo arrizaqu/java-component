06fcc7bc0c3997caf720e7240454909d31fe9a81
masyda.arrizaqu <arrizaqu@yahoo.com>
2016-03-02 22:11:23 +0700

    update name file postgress connector

---
+++ postgress_connector.java
@@ -0,0 +1,12 @@
+# Example Postgress Driver
+
+Class.forName("org.postgresql.Driver");
+Connection connection = null;
+connection = DriverManager.getConnection(
+   "jdbc:postgresql://hostname:port/dbname","username", "password");
+connection.close();
+
+
+
+file jdbc driver for pogressql : 
+http://jdbc.postgresql.org/download.html
