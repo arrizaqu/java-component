cbd98a48184b6c43777f701c6deade75869f88dc
masyda.arrizaqu <arrizaqu@yahoo.com>
2016-03-02 22:03:08 +0700

    udpate file mysql_connector.java

--- mysql_connector.java
+++ mysql_connector.java
@@ -1,8 +1,5 @@
-package com.inixindo;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLClientInfoException;
 import java.sql.SQLException;
 
 public class MysqlConnect {
@@ -12,14 +9,25 @@ public class MysqlConnect {
 	public Connection getConnection(){
 		
 		try{
-			Class.forName("com.mysql.jdbc.driver");
-			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/","mysql","");
+			Class.forName("com.mysql.jdbc.Driver");
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mysql","root","");
+			System.out.println("database connection oke..!!");
 		} catch(SQLException eSql){
 			eSql.printStackTrace();
+			System.out.println("connection failed");
 		} catch(Exception e){
 			e.printStackTrace();
+		} finally{
+			System.out.println("executed..!!");
 		}
 		
 		return this.connection;
+	}
+	
+	public static void main(String args[]){
+		MysqlConnect mysql = new MysqlConnect();
+		Connection con = mysql.getConnection();
+		//System.out.println("testing areas");
+		
 	}
 }
